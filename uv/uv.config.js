077b6ebe36d8f7f162b81680a3a682aafd7bef23
NjeShqiptar <hali001@sjacstudent.qld.edu.au>
2026-03-25 14:07:30 +1000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,9 +1,7 @@
 self.__uv$config = {
-    prefix: "/service/", // Changed from /active/service/
-    
-    // 👇 MAKE SURE THIS IS YOUR CORRECT RENDER URL 👇
-    wisp: "wss://proxy-backend-jf48.onrender.com//wisp/", 
-    
+    prefix: "/service/",
+    // 👇 YOUR ACTUAL BACKEND LINK 👇
+    wisp: "wss://proxy-backend-jf48.onrender.com/wisp/", 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
