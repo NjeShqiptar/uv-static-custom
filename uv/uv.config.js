db6ccb1d119ddf871e155148c1b3d302fa8726a2
NjeShqiptar <hali001@sjacstudent.qld.edu.au>
2026-03-25 04:41:52 +0000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
-    prefix: "/uv/service/",
+    prefix: "/uv-static-custom/service/",
     wisp: "wss://proxy-backend-jf48.onrender.com/wisp",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: "/uv-static-custom/uv/uv.handler.js",
+    client: "/uv-static-custom/uv/uv.client.js",
+    bundle: "/uv-static-custom/uv/uv.bundle.js",
+    config: "/uv-static-custom/uv/uv.config.js",
+    sw: "/uv-static-custom/uv/uv.sw.js",
 };
