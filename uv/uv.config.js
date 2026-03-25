a17b50091353577305db49fdb63463790c2fb5e2
NjeShqiptar <hali001@sjacstudent.qld.edu.au>
2026-03-25 19:45:39 +1000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
-    prefix: "/service/",        // UV proxy prefix (used in iframe)
-    wisp: "wss://proxy-backend-jf48.onrender.com/wisp",
+    prefix: "/service/",  // UV proxy prefix, intercepted by the service worker
+    wisp: "wss://proxy-backend-jf48.onrender.com/wisp", // Your Wisp backend
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "./uv/uv.handler.js",
     client: "./uv/uv.client.js",
     bundle: "./uv/uv.bundle.js",
     config: "./uv/uv.config.js",
-    sw: "./uv/uv.sw.js",
+    sw: "./uv/uv.sw.js"
 };
